394212dee5736f5bc2f0a1e591635fee4f0114bc
joseph dao <joedao55@gmail.com>
2020-07-18 23:35:32 -0400

    changed to FMT_COMPILE

--- src/img_util.cpp
+++ src/img_util.cpp
@@ -1,22 +1,22 @@
 #include "img_util.hpp"
 #include <fmt/format.h>
-
+#include <fmt/compile.h>
 std::string format_char_rgba(uint8_t *pixel, std::string_view c, const uint8_t opacity_cutoff)
 {
     if (pixel[3] > opacity_cutoff)
     {
-        return fmt::format("\x1b[38;2;{};{};{}m{}", pixel[0], pixel[1], pixel[2], c);
+        return fmt::format(FMT_COMPILE_H_("\x1b[38;2;{};{};{}m{}"), pixel[0], pixel[1], pixel[2], c);
     }
     return " ";
 }
 std::string format_char_rgb(uint8_t *pixel, std::string_view c)
 {
-    return fmt::format("\x1b[38;2;{};{};{}m{}", pixel[0], pixel[1], pixel[2], c);
+    return fmt::format(FMT_COMPILE_H_("\x1b[38;2;{};{};{}m{}"), pixel[0], pixel[1], pixel[2], c);
 }
 std::string format_char_g(uint8_t *pixel, std::string_view c)
 {
     uint8_t avg = static_cast<uint8_t>((pixel[0] + pixel[1] + pixel[2]) / 3);
-    return fmt::format("\x1b[38;2;{0};{0};{0}m{1}", avg, c);
+    return fmt::format(FMT_COMPILE_H_("\x1b[38;2;{0};{0};{0}m{1}"), avg, c);
 }
 
 std::string format_char_ga(uint8_t *pixel, std::string_view c, const uint8_t opacity_cutoff)
@@ -24,7 +24,7 @@ std::string format_char_ga(uint8_t *pixel, std::string_view c, const uint8_t opa
     uint8_t avg = static_cast<uint8_t>((pixel[0] + pixel[1] + pixel[2]) / 3);
     if (pixel[3] > opacity_cutoff)
     {
-        return fmt::format("\x1b[38;2;{0};{0};{0}m{1}", avg, c);
+        return fmt::format(FMT_COMPILE_H_("\x1b[38;2;{0};{0};{0}m{1}"), avg, c);
     }
     return " ";
 }
