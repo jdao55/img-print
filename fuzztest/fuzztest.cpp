b8ec2bebfdd403c26cd67bda57ec3d620932e287
joe dao <joedao55@gmail.com>
2020-09-18 20:18:44 -0400

    removed libvips form fuzz(causing linking errors)

--- fuzztest/fuzztest.cpp
+++ fuzztest/fuzztest.cpp
@@ -1,10 +1,12 @@
-#include "../src/img_print.hpp"
-#include "../src/img_util.hpp"
-
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+// #include "../src/img_print.hpp"
+// #include "../src/img_util.hpp"
+#include <cstdint>
+#include <cstddef>
+auto fuzztest(const uint8_t *Data, size_t Size)
 {
-    print_rgba(Data, Size, 1, "c");
-    print_rgb(Data, Size, 1, "c");
-    print_ga(Data, Size, 1, "c");
-    print_g(Data, Size, 1, "c");
+    if (Size > 0)
+        return Data[Size - 1];
+    return uint8_t(0);
 }
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) { fuzztest(Data, Size); }
